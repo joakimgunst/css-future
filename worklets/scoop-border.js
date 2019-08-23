03082a308247709049079ff08fd781e11322de6c
Joakim Gunst <joakim.gunst@nitor.com>
2019-08-23 15:19:53 +0300

    Bug fix and refactoring

--- worklets/scoop-border.js
+++ worklets/scoop-border.js
@@ -1,79 +1,83 @@
 class ScoopBorderPainter {
-    constructor() {
-        this.c = 0.5522847498307933984022516322796;
-    }
-
     static get inputProperties() {
         return ["--scoop-border-radius"];
     }
 
     /**
-     * 
-     * @param {CanvasRenderingContext2D} ctx 
-     * @param {{width: number; height: number;}} geom 
-     * @param {*} properties 
+     *
+     * @param {CanvasRenderingContext2D} ctx
+     * @param {{width: number; height: number;}} geom
+     * @param {*} properties
      */
     paint(ctx, geom, properties) {
         const color = "black";
-        const radius = this.getRadius(properties);
+        const radius = this.getRadius(geom, properties);
+        const { width, height } = geom;
 
         const points = [
             { x: radius, y: 0 },
-            { x: geom.width - radius, y: 0 },
-            { x: geom.width, y: radius },
-            { x: geom.width, y: geom.height - radius },
-            { x: geom.width - radius, y: geom.height },
-            { x: radius, y: geom.height },
-            { x: 0, y: geom.height - radius },
+            { x: width - radius, y: 0 },
+            { x: width, y: radius },
+            { x: width, y: height - radius },
+            { x: width - radius, y: height },
+            { x: radius, y: height },
+            { x: 0, y: height - radius },
             { x: 0, y: radius }
         ];
-        const bezierRadius = radius * this.c;
+
+        const bezierFactor = (4 / 3) * Math.tan(Math.PI / 8);
+        const bezierRadius = radius * bezierFactor;
 
         ctx.fillStyle = color;
         ctx.beginPath();
-        ctx.moveTo(points[0].x, points[0].y);
+        ctx.moveTo(radius, 0);
         ctx.lineTo(points[1].x, points[1].y);
         ctx.bezierCurveTo(
-            geom.width - radius,
+            width - radius,
             bezierRadius,
-            geom.width - bezierRadius,
+            width - bezierRadius,
             radius,
             points[2].x,
             points[2].y
         );
         ctx.lineTo(points[3].x, points[3].y);
         ctx.bezierCurveTo(
-            geom.width - bezierRadius,
-            geom.height - radius,
-            geom.width - radius,
-            geom.height - bezierRadius,
+            width - bezierRadius,
+            height - radius,
+            width - radius,
+            height - bezierRadius,
             points[4].x,
             points[4].y
         );
         ctx.lineTo(points[5].x, points[5].y);
         ctx.bezierCurveTo(
             radius,
-            geom.height - bezierRadius,
+            height - bezierRadius,
             bezierRadius,
-            geom.height - radius,
+            height - radius,
             points[6].x,
             points[6].y
         );
         ctx.lineTo(points[7].x, points[7].y);
-        ctx.bezierCurveTo(bezierRadius, radius, radius, bezierRadius, points[0].x, points[0].y);
+        ctx.bezierCurveTo(
+            bezierRadius,
+            radius,
+            radius,
+            bezierRadius,
+            points[0].x,
+            points[0].y
+        );
         ctx.closePath();
         ctx.fill();
     }
 
-    getRadius(properties) {
+    getRadius(geom, properties) {
         const radiusProperty = properties.get("--scoop-border-radius");
-
-        let radius;
         switch (radiusProperty.unit) {
             case "px":
                 return radiusProperty.value;
             case "percent":
-                return Math.min((radius * geom.width) / 100, geom.width / 2);
+                return (radiusProperty.value * geom.width) / 100;
             default:
                 throw new Error("Unsupported unit");
         }
